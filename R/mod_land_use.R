28e706baf43d95b94af43be9bb5847957dc9110c
Jadey Ryan <JRyan@agr.wa.gov>
2022-07-07 17:25:13 -0700

    changed to virtualSelectInput, added valueBoxes to land_use_tab

--- R/mod_land_use.R
+++ R/mod_land_use.R
@@ -7,41 +7,45 @@
 #' @noRd
 #'
 #' @importFrom shiny NS tagList
-#' @import shinyBS
-
-# TODO: add tooptip info boxes for valueBox to list what each category includes
+#' @importFrom shinyWidgets virtualSelectInput
+#' @importFrom shinydashboard box valueBoxOutput renderValueBox valueBox
 
 mod_land_use_ui <- function(id) {
   ns <- NS(id)
 
-counties <- as.character(unique(comet_wa$county))
-choices <- c("All Counties", counties)
-
   tagList(
-    includeCSS("inst/app/www/css/style.css"),
-    tabsetPanel(
-      type = "pills",
-      tabPanel("Total Acres by County", br(),
-          fluidRow(column(
-            3, selectizeInput(
-              inputId = ns("county"),
-              label = "County",
-              choices = choices,
-              multiple = TRUE,
-              options = list(
-                plugins = list("remove_button"),
-                placeholder = "Pick your counties"
-              )
-            ),
-            actionButton(ns("reset"), "Reset")
+    fluidRow(
+        box(
+          title = strong("Agricultural Acres by County"),
+          width = 3, status = "primary", collapsible = TRUE, solidHeader = TRUE,
+          virtualSelectInput(
+            inputId = ns("county"),
+            label = strong("County"),
+            choices = unique(comet_wa$county),
+            selected = unique(comet_wa$county),
+            multiple = TRUE,
+            search = TRUE,
+            position = "bottom",
+            optionsCount = 5
           ),
-          column(3, wellPanel(style = "height:100%", htmlOutput(ns("cropland")))),
-          column(3, wellPanel(style = "height:100%", htmlOutput(ns("pasture")))),
-          column(3, wellPanel(style = "height:100%", htmlOutput(ns("other")))),
-               )
-          ),
-      tabPanel("Crop Group Map", br(), htmlOutput(ns("crop_map"))),
-      tabPanel("Major Land Resource Area Map", br(), htmlOutput(ns("mlra_map")))
+          valueBoxOutput(ns("cropland"), width = 12),
+          valueBoxOutput(ns("pasture"), width = 12),
+          valueBoxOutput(ns("other"), width = 12)
+      ),
+      column(
+        width = 9,
+        box(
+          title = strong("Maps"),
+          width = NULL, status = "primary", collapsible = TRUE, solidHeader = TRUE,
+        tabsetPanel(
+          type = "pills",
+          tabPanel("Crop Group Map", icon = icon("carrot"), br(),
+                   htmlOutput(ns("crop_map"))),
+          tabPanel("Major Land Resource Area Map", icon = icon("mountain"), br(),
+                   htmlOutput(ns("mlra_map")))
+        )
+        )
+      )
     )
   )
 }
@@ -53,57 +57,58 @@ mod_land_use_server <- function(id) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
-    counties <- as.character(unique(comet_wa$county))
-    choices <- c("All Counties", counties)
-
-    # if user selects "All Counties", input will update to select all choices besides "All Counties"
-
-    observe({
-      if ("All Counties" %in% input$county) {
-        selected_choices <- setdiff(choices, "All Counties")
-        updateSelectizeInput(session, "county", selected = selected_choices)
-      }
-    })
-
-    # reset filter on Reset button click
-
-    observeEvent(input$reset, {
-      shinyjs::reset("county")
-    })
-
     # cropland indicator
 
-    output$cropland <- renderText({
-      paste(
-        "<b><center>Cropland Acres</b>",
-        "<br>",
-        fct_sumAcres(county = input$county, type = "cropland"),
-        "<br>",
-        "Excludes developed, other, pasture, shellfish, and turfgrass.</center>"
+    value_cropland <- reactive({
+      value_cropland <- fct_sumAcres(county = input$county, type = "cropland") %>%
+        prettyNum(big.mark = ",")
+    })
+
+    output$cropland <- renderValueBox({
+      valueBox(subtitle = HTML("<b>Cropland Acres</b> <br>
+                                All crop groups except developed,
+                                other, pasture, shellfish, and turfgrass."),
+              value = paste(value_cropland(), "Ac"),
+              icon = icon("apple"),
+              color = "red",
+              width = 12
       )
     })
 
     # pasture indicator
 
-    output$pasture <- renderText({
-      paste(
-        "<b><center>Pasture Acres</b>",
-        "<br>",
-        fct_sumAcres(county = input$county, type = "pasture"),
-        "<br>",
-        "Includes only pasture.</center>"
+    # "Includes only pasture.</center>"
+
+    value_pasture <- reactive({
+      value_cropland <- fct_sumAcres(county = input$county, type = "pasture") %>%
+        prettyNum(big.mark = ",")
+    })
+
+    output$pasture <- renderValueBox({
+      valueBox(subtitle = HTML("<b>Pasture Acres</b> <br>
+                               Includes pasture only."),
+               value = paste(value_pasture(), "Ac"),
+               icon = icon("horse"),
+               color = "yellow",
+               width = 12
       )
     })
 
     # other indicator
 
-    output$other <- renderText({
-      paste(
-        "<b><center>Other Ag Acres</b>",
-        "<br>",
-        fct_sumAcres(county = input$county, type = "other"),
-        "<br>",
-        "Includes only conservation, fallow, research, unknown, and wildlife feed.</center>"
+    value_other <- reactive({
+      value_cropland <- fct_sumAcres(county = input$county, type = "other") %>%
+        prettyNum(big.mark = ",")
+    })
+
+    output$other <- renderValueBox({
+      valueBox(subtitle = HTML("<b>'Other' Ag Acres</b> <br>
+                               Includes conservation, fallow,
+                               wildlife feed, research, unknown, and alkali bee bed"),
+               value = paste(value_other(), "Ac"),
+               icon = icon("leaf"),
+               color = "green",
+               width = 12
       )
     })
 
@@ -112,7 +117,7 @@ mod_land_use_server <- function(id) {
     output$crop_map <- renderUI({
       tags$iframe(
         src = "https://nras.maps.arcgis.com/apps/instant/interactivelegend/index.html?appid=1e0f1a416e1b4f9ea0dfb10b82e53da7",
-        height = "600px",
+        height = "500px",
         width = "100%"
       )
     })
@@ -122,7 +127,7 @@ mod_land_use_server <- function(id) {
     output$mlra_map <- renderUI({
       tags$iframe(
         src = "https://nras.maps.arcgis.com/apps/instant/interactivelegend/index.html?appid=47fe1baea32e46248c3fb8e6958a1c30",
-        height = "600px",
+        height = "500px",
         width = "100%"
       )
     })
